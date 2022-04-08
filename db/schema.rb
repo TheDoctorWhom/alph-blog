8eabdb0e62aec3a77646ecc47f5bc3cbb62d8242
TheDoctorWhom <Zak.dutton@gmail.com>
2022-04-08 12:42:32 -0600

    added Article varibles to article page

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_07_205834) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_07_210524) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "description"
